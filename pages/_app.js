575135300d03ebe0982e809bfef4374aa2c9b568
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-07-21 22:54:10 +0200

    setup material ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,38 @@
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import { AuthProvider } from '../contexts/Auth';
+import theme from '../styles/materialUI/theme';
 
-const App = ({ Component, pageProps }) => (
-  <>
-    <Head>
-      <title>Home</title>
-    </Head>
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
-  </>
-);
+const App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <AuthProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </AuthProvider>
+    </>
+  );
+};
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
 
 export default App;
