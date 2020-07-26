fb7ae7429485b9fa932121de22de3cba0b87bd19
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-07-27 00:00:26 +0200

    rename useUser hook

--- hooks/useUser.js
+++ hooks/useUser.js
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import { AuthContext } from '../contexts/Auth';
 
-export const useSession = () => {
+export const useUser = () => {
   const { user } = useContext(AuthContext);
   return user;
 };
